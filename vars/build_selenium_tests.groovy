d1b680ef9f56ad23fe97561decb33c6b172aeba7
Harsh Vashishth <144763467+hrsh-1294@users.noreply.github.com>
2026-04-05 21:07:59 +0530

    Refactor call function to accept custom path

--- vars/build_selenium_tests.groovy
+++ vars/build_selenium_tests.groovy
@@ -1,5 +1,7 @@
-def call() {
-    dir('selenium-tests') {
-        sh 'mvn clean install'
+def call(String path = 'selenium-tests') {
+    docker.image('maven:3.9.9-eclipse-temurin-17').inside {
+        dir(path) {
+            sh 'mvn clean install'
+        }
     }
 }
