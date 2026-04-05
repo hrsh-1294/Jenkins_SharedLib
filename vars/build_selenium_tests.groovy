1dff64fb4110385066598d7fc0b4db363210811e
Harsh Vashishth <144763467+hrsh-1294@users.noreply.github.com>
2026-04-05 21:10:51 +0530

    Update build_selenium_tests.groovy

--- vars/build_selenium_tests.groovy
+++ vars/build_selenium_tests.groovy
@@ -1,7 +1,5 @@
-def call(String path = 'selenium-tests') {
-    docker.image('maven:3.9.9-eclipse-temurin-17').inside {
-        dir(path) {
-            sh 'mvn clean install'
-        }
+def call() {
+    dir('selenium-tests') {
+        sh 'mvn clean install'
     }
 }
