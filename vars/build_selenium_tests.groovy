9e969f5db1a8ec63d66ce2811221a697e69f6d0d
Harsh Vashishth <144763467+hrsh-1294@users.noreply.github.com>
2026-04-05 21:19:27 +0530

    Refactor call function to simplify parameters

--- vars/build_selenium_tests.groovy
+++ vars/build_selenium_tests.groovy
@@ -1,7 +1,7 @@
-def call(steps, String path = 'selenium-tests') {
-    steps.docker.image('maven:3.9.9-eclipse-temurin-17').inside {
-        steps.dir(path) {
-            steps.sh 'mvn clean install -DskipTests'
+def call(String path = 'selenium-tests') {
+    docker.image('maven:3.9.9-eclipse-temurin-17').inside {
+        dir(path) {
+            sh 'mvn clean install -DskipTests'
         }
     }
 }
