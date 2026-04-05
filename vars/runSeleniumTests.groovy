3415965787912a2f0e742ad033f22c53c96c38a4
Harsh Vashishth <144763467+hrsh-1294@users.noreply.github.com>
2026-04-05 22:20:17 +0530

    Add cleanup for Selenium test container

--- vars/runSeleniumTests.groovy
+++ vars/runSeleniumTests.groovy
@@ -1,3 +1,7 @@
 def call() {
-    sh 'docker compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from tests'
+    try {
+        sh 'docker compose -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from tests'
+    } finally {
+        sh 'docker compose -f docker-compose.test.yml down'
+    }
 }
