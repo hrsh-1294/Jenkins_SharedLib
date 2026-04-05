b8a93f9506603b9fcc878d632e8ecf5fb1094f64
Harsh Vashishth <144763467+hrsh-1294@users.noreply.github.com>
2026-04-05 20:58:23 +0530

    Enhance SonarQube analysis script with additional parameters
    Updated sonar-scanner command to include sources and binaries.

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,11 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+      sh """
+      ${SONAR_HOME}/bin/sonar-scanner \
+      -Dsonar.projectName=${Projectname} \
+      -Dsonar.projectKey=${ProjectKey} \
+      -Dsonar.sources=. \
+      -Dsonar.java.binaries=selenium-tests/target/test-classes
+      """
   }
 }
